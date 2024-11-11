4ed092596a6e2d916a2b34ab7ef93eadd8b915ea
Sayam Alvi <sayamalvi07@gmail.com>
2024-11-11 18:29:59 +0530

    add config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default tseslint.config(
             },
         },
         rules: {
-            'no-console': 'error',
+            // 'no-console': 'error',
             'dot-notation': 'error',
         },
     },
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-function welcome(name: string) {
-    // console.log('Hello');
-    // console.log("Hi")
-    const user = {
-        name: 'Sayam',
-    };
-    const fname = user.name;
-    return name + fname;
-}
+import { Config } from './config';
 
-welcome('sayam');
+console.log(Config.PORT);
