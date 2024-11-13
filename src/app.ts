b02bb25f9cf41a622fad22f6057578b469c8bac2
Sayam Alvi <sayamalvi07@gmail.com>
2024-11-13 19:13:09 +0530

    add some auth tests

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 import logger from './config/logger';
 import { HttpError } from 'http-errors';
+import authRouter from './routes/auth';
 
 const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome to auth service');
 });
+app.use('/auth', authRouter);
+
 app.use((err: HttpError, req: Request, res: Response) => {
     logger.error(err.message);
     const statusCode = err.statusCode || 500;
---
+++ tests/users/register.spec.ts
@@ -0,0 +1,49 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+describe('POST /auth/register', () => {
+    describe('Given all fields', () => {
+        it('should return the 201 status code', async () => {
+            // AAA-> Arrange, Act, Assert
+
+            // Arrange
+            const userData = {
+                firstName: 'Sayam',
+                lastName: 'Alvi',
+                email: 'sayamalvi07@gmail.com',
+                password: 'secret',
+            };
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('should return valid json response', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Sayam',
+                lastName: 'Alvi',
+                email: 'sayamalvi07@gmail.com',
+                password: 'secret',
+            };
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+
+            expect(response.headers['content-type']).toEqual(
+                expect.stringContaining('json'),
+            );
+        });
+    });
+    describe('Fields are missing', () => {});
+});
