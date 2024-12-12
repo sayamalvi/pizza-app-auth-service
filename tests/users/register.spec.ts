a299fa134fc74e69cf196deec15c44ca139b1742
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-12 17:29:25 +0530

    remove password from get users response

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -131,7 +131,7 @@ describe('POST /auth/register', () => {
             await request(app).post('/auth/register').send(userData);
 
             const userRepository = connection.getRepository(User);
-            const users = await userRepository.find();
+            const users = await userRepository.find({ select: ['password'] });
             expect(users[0].password).not.toBe(userData.password);
             expect(users[0].password).toHaveLength(60);
             expect(users[0].password).toMatch(/^\$2[a|b]\$\d+\$/);
